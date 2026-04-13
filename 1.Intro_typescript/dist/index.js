943b9350668879c04097de38fb0379dfa3d74b51
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-13 13:49:10 +0600

    typescript intro & basic types included

---
+++ 1.Intro_typescript/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name}`;
+}
+console.log(greet("Jakaria"));
+//# sourceMappingURL=index.js.map
---
+++ 2.basic_types/src/index.ts
@@ -0,0 +1,15 @@
+let name: string = "Arafat";
+
+// Type 'boolean' is not assignable to type 'string'.
+// name = true;
+
+// Type 'number' is not assignable to type 'string'.
+// name = 25;
+name = "Rohim";
+console.log(name);
+
+const rollNumber: number = 25;
+console.log(rollNumber);
+
+const isPassed: boolean = true;
+console.log(isPassed);
