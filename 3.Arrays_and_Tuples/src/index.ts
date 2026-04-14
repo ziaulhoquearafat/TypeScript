5cc08dea4b59562dbbf2407aa4d59bc820ef02a4
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-15 00:12:05 +0600

    array

---
+++ 3.Arrays_and_Tuples/src/index.ts
@@ -0,0 +1,12 @@
+// Array
+
+const list: number[] = [10, 15, 25];
+const names: string[] = ["Hablu", "bablu", "dablu"];
+console.log(list[0]);
+console.log(names[2]);
+
+// alternative methode
+const list2: Array<number> = [80, 52, 45];
+console.log(list2[2]);
+const names2: Array<string> = ["montu", "sontu", "fantu"];
+console.log(names[0]);
