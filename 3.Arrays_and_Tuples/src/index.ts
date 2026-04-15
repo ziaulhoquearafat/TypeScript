5d19aa2984e514015a3258a9795b7e98da222b2f
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-15 19:57:25 +0600

    added tuples

---
+++ 3.Arrays_and_Tuples/src/index.ts
@@ -0,0 +1,17 @@
+// Array
+
+const list: number[] = [10, 15, 25];
+const names: string[] = ["Hablu", "bablu", "dablu"];
+console.log(list[0]);
+console.log(names[2]);
+
+// alternative methode
+const list2: Array<number> = [80, 52, 45];
+console.log(list2[2]);
+const names2: Array<string> = ["montu", "sontu", "fantu"];
+console.log(names[0]);
+
+// tuples data type - here we know the value ex: student data
+
+const studentInfo: [string, number, boolean] = ["Karim", 21, true];
+console.log(studentInfo[1]);
