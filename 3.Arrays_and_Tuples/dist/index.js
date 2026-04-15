1ddd1b8823931b71fd814c6d378954537b8b8c5d
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-15 20:16:53 +0600

    arrays and tuples added

---
+++ 3.Arrays_and_Tuples/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// Array
+Object.defineProperty(exports, "__esModule", { value: true });
+const list = [10, 15, 25];
+const names = ["Hablu", "bablu", "dablu"];
+console.log(list[0]);
+console.log(names[2]);
+// alternative methode
+const list2 = [80, 52, 45];
+console.log(list2[2]);
+const names2 = ["montu", "sontu", "fantu"];
+console.log(names[0]);
+// tuples data type - here we know the value ex: student data
+const studentInfo = ["Karim", 21, true];
+console.log(studentInfo[1]);
+//# sourceMappingURL=index.js.map
