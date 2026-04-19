3330a0b36ad1628946463d29426d7ef1f8392e49
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-19 17:33:01 +0600

    noted type alias

--- 11.Object_Types_&_Type_Aliases/src/index.ts
+++ 11.Object_Types_&_Type_Aliases/src/index.ts
@@ -10,3 +10,34 @@ function printData(user: { name: string; age: number }) {
 }
 
 printData(student);
+
+// ------------------------ Type Alias -----------------------------
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const student1: User = {
+  name: "Habib",
+  age: 42,
+};
+
+function showUser(user: User): void {
+  console.log(`${user.name}`);
+}
+showUser(student1);
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
+
+const product1: Product = {
+  id: 1,
+  name: "MotherBoard",
+  description: "China Motherboard",
+  price: 10000,
+};
