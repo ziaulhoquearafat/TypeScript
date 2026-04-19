b2f59e9a6ce3598eadf18df9b74200e22cf9dabb
Md Ziaul Haque Arafat <85094984+ziaulhoquearafat@users.noreply.github.com>
2026-04-19 15:56:47 +0600

    Delete 9.Return_Type/dist directory

--- 9.Return_Type/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// never return type
-Object.defineProperty(exports, "__esModule", { value: true });
-function throwError() {
-    throw new Error("Something went wrong");
-}
-console.log(throwError());
-//# sourceMappingURL=index.js.map
