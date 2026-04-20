dc4bc3bcfec7597756ba54906c6802f3e891962f
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-20 21:32:45 +0600

    noted constructor and modifiers

--- 21.OOP_constructor_and_modifires/src/index.ts
+++ 21.OOP_constructor_and_modifires/src/index.ts
@@ -20,3 +20,30 @@ user.greet();
 const user2 = new Person("Bablu", 49);
 console.log(user2.name);
 user2.greet();
+
+// modifires (public, private, protected)
+// ? public: access from anywhere
+// ? private: only access from classes
+// ? protected: access from class and subclasses
+
+class BankAccount {
+  public accountName: string;
+  private balance: number;
+  protected accountType: string;
+
+  constructor(name: string, amount: number) {
+    this.accountName = name;
+    this.balance = amount;
+    this.accountType = "Savings";
+  }
+
+  public showBalance() {
+    console.log(`Your balance is ${this.balance}`);
+  }
+}
+
+// create user
+const accountOne = new BankAccount("Alice", 5000);
+accountOne.showBalance();
+
+console.log(accountOne.accountName);
