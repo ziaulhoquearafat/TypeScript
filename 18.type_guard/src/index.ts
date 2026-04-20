85c12260eda4df5d99350e6a14bfc6005eee8436
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-20 17:56:13 +0600

    noted type guard in and instanceof

--- 18.type_guard/src/index.ts
+++ 18.type_guard/src/index.ts
@@ -11,3 +11,63 @@ function display(value: string | number): void {
 }
 display("Hello and Welcome");
 display(493.456912359);
+
+// in operator Type Guard
+
+type Car = {
+  drive: () => void;
+};
+
+type Boat = {
+  seil: () => void;
+};
+
+function move(vehicle: Car | Boat) {
+  if ("drive" in vehicle) {
+    vehicle.drive();
+  } else {
+    vehicle.seil();
+  }
+}
+
+const car: Car = {
+  drive: () => {
+    console.log("Car is driving");
+  },
+};
+
+const boat: Boat = {
+  seil: () => {
+    console.log("Boat is seiling");
+  },
+};
+
+move(car);
+move(boat);
+
+// instanceof Type Guard
+
+class Dog {
+  bark() {
+    console.log("Woof Woof");
+  }
+}
+
+class Cat {
+  meow() {
+    console.log("meow meow");
+  }
+}
+
+function makeSound(animal: Dog | Cat) {
+  if (animal instanceof Dog) {
+    animal.bark();
+  } else {
+    animal.meow();
+  }
+}
+
+const dog = new Dog();
+const cat = new Cat();
+makeSound(dog);
+makeSound(cat);
