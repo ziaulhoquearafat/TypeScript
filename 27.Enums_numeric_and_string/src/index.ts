66650f5427999294a25b08a42ab888cb06928d2e
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-22 19:17:33 +0600

    const enum noted with use cases

--- 27.Enums_numeric_and_string/src/index.ts
+++ 27.Enums_numeric_and_string/src/index.ts
@@ -29,3 +29,21 @@ enum Status {
 
 const res: Status = Status.Pending;
 console.log(res);
+
+// cosnt enum
+
+const enum Size {
+  Small,
+  Medium,
+  Large,
+}
+
+let s1 = Size.Medium;
+console.log(s1);
+
+// Use-Cases
+// 1. Direction or Position: enum Direction (Up, Down, Left, Right}
+// 2. Roles or permissions: enum Role {Guest, User, Admin}
+// 3. HTTP Status or APIs: enum HttpStatus (0K = 2øø, NotFound)
+// 4. Days of week or months: enum Days {Mon, Tue, Wed, Thu, Fri}
+// 5. Toggle states or modes: enum Mode { Dark, Light)
