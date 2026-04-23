f2d650b56c329b7bab39e7b09774ce92b4b9a811
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-23 18:48:46 +0600

    useSate and useEffect with typescript

--- 29.react+typescript/src/App.tsx
+++ 29.react+typescript/src/App.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import Greetings from "./components/Greetings";
 
 function App() {
   const name = "Hablu";
   const age = 48;
 
+  const [count, setCount] = useState<number>(0);
+
+  const [user, setUser] = useState<{ name: string; email: string } | null>(
+    null,
+  );
+
+  useEffect(() => {
+    setUser({
+      name: "Alice",
+      email: "alice@gmail.com",
+    });
+  }, []);
+
   return (
     <>
       <h1>Welcome to React + TypeScript</h1>
       <Greetings name={name} age={age} />
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increament</button>
+      {user && (
+        <p>
+          User: {user.name} - {user.email}
+        </p>
+      )}
     </>
   );
 }
