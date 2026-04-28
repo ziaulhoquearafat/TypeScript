ea5e8ee135da31c3a8f1ef53c3b8867029764938
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-28 22:39:37 +0600

    types added

--- 31.Todo-project/src/App.tsx
+++ 31.Todo-project/src/App.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 function App() {
+  const [newTodo, setNewTodo] = useState<string>("");
+  console.log(newTodo);
+
   return (
-    <>
-      <h1>Hello Todo App</h1>
-    </>
+    <div className="min-h-screen bg-gray-300 py-5">
+      <div className="max-w-md mx-auto py-2">
+        <h1 className="font-bold text-2xl text-center">Todo App</h1>
+        <form className="pb-3">
+          <div className="flex gap-2 mt-3 items-center justify-center">
+            <input
+              type="text"
+              placeholder="Add new Todo..."
+              className="border py-2 rounded px-3 flex-1 focus:outline-0 ring-1"
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 cursor-pointer rounded"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+        {/*  display todo items */}
+        <div>Todo List</div>
+      </div>
+    </div>
   );
 }
 
--- 32.Dashboard_project/src/App.tsx
+++ 32.Dashboard_project/src/App.tsx
@@ -1,10 +1,11 @@
 function App() {
   return (
-    <>
-      <div>
-        <h1>a dashboard project</h1>
+    <div className="min-h-screen bg-gray-200 py-8">
+      <div className="max-w-7xl mx-auto px-4">
+        <h1 className="text-center text-2xl font-bold">User Dashboard</h1>
+        <p>User List</p>
       </div>
-    </>
+    </div>
   );
 }
 
